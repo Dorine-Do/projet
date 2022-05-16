3dd58ae7542cb61902d27a6b6bc5d07dad9d412b
Dorine-Do <dorine.journet@laposte.net>
2022-05-16 11:52:40 +0200

    QuestionController.php refacto dans InstructorController.php

--- src/Controller/InstructorController.php
+++ src/Controller/InstructorController.php
@@ -2,15 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InstructorController extends AbstractController
 {
-    #[Route('/instructor', name: 'app_instructor')]
-    public function index(): Response
+    private $repository;
+
+    public function __construct(QuestionRepository $repository){
+        $this->repository = $repository;
+    }
+//
+//    #[Route('/instructor', name: 'app_instructor')]
+//    public function index(): Response
+//    {
+//        return $this->render('instructor/index.html.twig', [
+//            'controller_name' => 'InstructorController',
+//        ]);
+//    }
+
+    /**
+     * @Route("/quest", name="question.index")
+     * @return Response
+     */
+    public function displayQuestions(): Response
+
     {
+        $question = $this->repository->findALlVisible();
+        dump($question);
         return $this->render('instructor/index.html.twig', [
             'controller_name' => 'InstructorController',
         ]);
