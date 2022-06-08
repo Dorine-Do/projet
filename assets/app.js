e06b49544341bd0dcdf77619ac6e752ce74829f6
Dorine-Do <dorine.journet@laposte.net>
2022-06-08 16:17:33 +0200

    Base de React

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+import Home from './js/components/Home';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+    root.render(
+        <h1>Hello, world!</h1>
+    );
