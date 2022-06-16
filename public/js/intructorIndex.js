34fdca0863125bf9d3266df37e93ef5c0ac6aa36
Dorine-Do <dorine.journet@laposte.net>
2022-06-16 18:54:12 +0200

    correction bug js display_question

--- public/js/intructorIndex.js
+++ public/js/intructorIndex.js
@@ -1,27 +1,44 @@
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log(proposals)
-    $status = false; // Chevron fermé
+
+    let div_proposals = document.querySelectorAll('.div_proposals')
+    div_proposals.forEach(div => {
+        div.classList.add('display_none')
+    })
+
     let p_prop;
     let chevrons = document.querySelectorAll('.img_chevron')
-    console.log(chevrons)
+    // console.log(chevrons)
     chevrons.forEach(chevron=>{
+
         chevron.addEventListener('click',(e)=>{
-            if($status === false){ // Si fermé alors
-                let id = chevron.dataset.id
-                let div_question = e.target.parentElement.parentElement.parentElement
-                $div_proposals = div_question.querySelector('.div_proposals')
+
+            console.log(e) // { 2 : false} // false
+
+            let div_question = e.target.parentElement.parentElement.parentElement
+            let div_js = div_question.querySelector('.div_js')
+            // console.log(div_js)
+            let status = (e.target.dataset.status === 'true') // return false un boolean si status !== 'true' et true si === true
+            console.log(status)
+            if(status === false ){ // Si fermé alors
+                console.log("YOUPI!!!!!!!!!")
+
                 for (const proposal of proposals) {
-                    if(id == proposal.id_question){
+                    // console.log(proposal.id_question)
+                    // console.log(id)
+                    let id = parseInt(e.target.dataset.id)
+                    if(id === proposal.id_question){
+                        console.log("yeah")
                         p_prop = document.createElement('p')
                         p_prop.innerHTML = proposal.wording
-                        $div_proposals.append(p_prop);
+                        div_js.append(p_prop);
                     }
                 }
-                $status = true; // Chevron ouvert
-            }else{ // si ouvert alors
-                $div_proposals.innerHTML="";
-                $status = false;
+                e.target.dataset.status = true; // Chevron ouvert
+            }
+            else{ // si ouvert alors
+                div_js.innerHTML="";
+                e.target.dataset.status = false;
             }
         })
     })
