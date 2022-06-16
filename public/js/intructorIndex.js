44e9602afa997ecc3460e65c3e42e1a99d3b870d
Dorine-Do <dorine.journet@laposte.net>
2022-06-16 19:22:26 +0200

    js mise en page instructoIndex.js

--- public/js/intructorIndex.js
+++ public/js/intructorIndex.js
@@ -1,34 +1,32 @@
 
 document.addEventListener("DOMContentLoaded", (event) => {
 
-    let div_proposals = document.querySelectorAll('.div_proposals')
+    let div_proposals = document.querySelectorAll('.div_proposals');
+
     div_proposals.forEach(div => {
         div.classList.add('display_none')
     })
 
     let p_prop;
-    let chevrons = document.querySelectorAll('.img_chevron')
-    // console.log(chevrons)
+    let chevrons = document.querySelectorAll('.img_chevron');
+
     chevrons.forEach(chevron=>{
 
         chevron.addEventListener('click',(e)=>{
 
-            console.log(e) // { 2 : false} // false
-
             let div_question = e.target.parentElement.parentElement.parentElement
-            let div_js = div_question.querySelector('.div_js')
-            // console.log(div_js)
-            let status = (e.target.dataset.status === 'true') // return false un boolean si status !== 'true' et true si === true
-            console.log(status)
-            if(status === false ){ // Si fermé alors
-                console.log("YOUPI!!!!!!!!!")
+            let div_js = div_question.querySelector('.div_js');
+            // return false un boolean si status !== 'true' et true si === true
+            let status = (e.target.dataset.status === 'true');
+
+            if( status === false ){ // Si fermé alors
 
                 for (const proposal of proposals) {
-                    // console.log(proposal.id_question)
-                    // console.log(id)
-                    let id = parseInt(e.target.dataset.id)
-                    if(id === proposal.id_question){
-                        console.log("yeah")
+
+                    let id = parseInt(e.target.dataset.id);
+
+                    if( id === proposal.id_question ) {
+
                         p_prop = document.createElement('p')
                         p_prop.innerHTML = proposal.wording
                         div_js.append(p_prop);
@@ -36,7 +34,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 }
                 e.target.dataset.status = true; // Chevron ouvert
             }
-            else{ // si ouvert alors
+            else { // si ouvert alors
                 div_js.innerHTML="";
                 e.target.dataset.status = false;
             }
