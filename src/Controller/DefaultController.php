19f0aa575325841b53634d7da25cdca94dd3bf49
matth <matthieu.fergola@gmail.com>
2022-07-05 11:42:19 +0200

    fichiers locaux

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,12 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
 
-    /**
-     * @Route("{reactRouting}", priority="-1" , name="home", defaults={"reactRouting": null}, requirements={"reactRouting"=".+"})
-     */
 //    /**
-//     * @Route("/", name="home")
-//     */
+//     * @Route("{reactRouting}", priority="-1" , name="home", defaults={"reactRouting": null}, requirements={"reactRouting"=".+"})
+//    */
+    /**
+     * @Route("/", name="home")
+     */
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
@@ -24,54 +24,55 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/instucteur/questions", name="display_instructeur_questions")
-     */
-    public function getQuestionsByIdAuthor(QuestionRepository $questionRepository, ProposalRepository $proposalRepository)
-    {
-        // findBy returns an array of Instances's Entity = ARRAY
-        // find return ONE Instance's Entity = OBJECT
-        $questions = $questionRepository->findBy(["id_author" => 2]);
-
-        $questionOrders = [];
-        $resumeProposal = [];
-
-        foreach ($questions as $question) {
-            $question_id = $question->getId();
-            $questionOrder = [
-                'id' => $question_id,
-                'wording' => $question->getWording(),
-                'difficulty' => $question->getDifficulty(),
-                'module' => $question->getModule(),
-                'enabled' => $question->getEnabled()
-            ];
-            array_push($questionOrders, $questionOrder);
-
-            $proposals[$question_id] = $proposalRepository->findBy(['question' => $question_id]);
-            foreach ($proposals[$question_id] as $proposal){
-                $proposalValues = [
-                    'id'=>$proposal->getId(),
-                    'wording'=>$proposal->getWording(),
-                    'id_question'=>$proposal->getQuestion()->getId()
-                ];
-                array_push($resumeProposal, $proposalValues);
-            }
-        }
+ //   /**
+ //    * @Route("/instucteur/questions", name="display_instructeur_questions")
+ //    */
+ //   public function getQuestionsByIdAuthor(QuestionRepository $questionRepository, ProposalRepository $proposalRepository)
+ //   {
+ //       // findBy returns an array of Instances's Entity = ARRAY
+ //       // find return ONE Instance's Entity = OBJECT
+//        $questions = $questionRepository->findBy(["id_author" => 2]);
+//
+ //       $questionOrders = [];
+ //       $resumeProposal = [];
+//
+ //       foreach ($questions as $question) {
+ //           $question_id = $question->getId();
+ //           $questionOrder = [
+ //               'id' => $question_id,
+ //               'wording' => $question->getWording(),
+ //               'difficulty' => $question->getDifficulty(),
+ //               'module' => $question->getModule(),
+  //              'enabled' => $question->getEnabled()
+ //          ];
+ //           array_push($questionOrders, $questionOrder);
+//
+ //           $proposals[$question_id] = $proposalRepository->findBy(['question' => $question_id]);
+ //           foreach ($proposals[$question_id] as $proposal){
+ //               $proposalValues = [
+ //                   'id'=>$proposal->getId(),
+ //                   'wording'=>$proposal->getWording(),
+ //                   'id_question'=>$proposal->getQuestion()->getId()
+ //               ];
+  //              array_push($resumeProposal, $proposalValues);
+ //           }
+  //      }
 //        dd($questionOrder);
-        $proposals = ['proposals' => $resumeProposal];
-        $questions = ['questions' => $questionOrders];
-        $questionsProposals = array_merge($questions,$proposals);
+ //       $proposals = ['proposals' => $resumeProposal];
+ //       $questions = ['questions' => $questionOrders];
+ //       $questionsProposals = array_merge($questions,$proposals);
 //        dd($questionsProposals);
 
-        $response = new Response();
+ //       $response = new Response();
 
-        /*TODO : réparer l'erreur : Blocage d’une requête multiorigines (Cross-Origin Request) */
-        $response->headers->set('Content-Type', 'application/json');
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+ //       /*TODO : réparer l'erreur : Blocage d’une requête multiorigines (Cross-Origin Request) */
+ //       $response->headers->set('Content-Type', 'application/json');
+ //       $response->headers->set('Access-Control-Allow-Origin', '*');
 
-        $response->setContent(json_encode($questionsProposals));
+//        $response->setContent(json_encode($questionsProposals));
 //        $response->setContent(json_encode($resumeProposal));
 
-        return $response;
-    }
+//        return $response;
+ //   }
+
 }
