824f2a2282f64a98d411f7e94d5c4e11ec380cb4
matth <matthieu.fergola@gmail.com>
2022-07-07 11:14:06 +0200

    Fixtures retravaillées

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Enum\Difficulty;
 use App\Entity\Instructor;
+use App\Entity\LinkSessionModule;
 use App\Entity\LinkSessionStudent;
 use App\Entity\Module;
 use App\Entity\Proposal;
@@ -55,149 +56,160 @@ class AppFixtures extends Fixture
         $ChoicesDifficulty = [ Difficulty::Easy, Difficulty::Medium, Difficulty::Difficult];
 
         $faker = Faker\Factory::create();
-//        //Module
-//        for ($i=0; $i<10; $i++){
-//
-//            $word = $faker->word();
-//            $module = new Module();
-//            $module->setTitle($word);
-//            $module->setNumberOfWeeks(rand(1,10));
-//
-//            $manager->persist($module);
-////            $manager->flush();
-//
-//            array_push($arrayModule, $module);
-//
-//        }
-//
-//        //Session
-//        for ($i=0; $i<10; $i++){
-//
-//            $word = $faker->word();
-//            $session = new Session();
-//            $session->setName($word);
-//            $session->setSchoolYear(2021);
-//
-//            $manager->persist($session);
-////            $manager->flush();
-//
-//            array_push($arraySession, $session);
-//        }
-//
-//        //Instructeur
-//        for ($i=0; $i<10; $i++){
-//            $name = $faker->name();
-//            $arrayName = explode(' ', $name);
-//            $instructor = new Instructor();
-//
-//            $instructor->setFirstname($arrayName[0]);
-//            $instructor->setLastname($arrayName[1]);
-//            $instructor->setEmail($faker->email());
-//            $instructor->setPhoneNumber('06xxxxxxxxxx');
-//            $instructor->setBirthDate(new \DateTime("22-05-1957"));
-//            $instructor->setRoles(['instructor']);
-//
-//            $instructor->setPassword(
-//                $this->userPasswordHasherInterface->hashPassword(
-//                    $instructor, "password"
-//                )
-//            );
-//
-//            $instructor->addModule($arrayModule[rand(1,9)]);
-////            $sessionInstructor = $arraySession[rand(1,9)];
-////            $instructor->addSession($sessionInstructor);
-//
-//            $manager->persist($instructor);
-////            $manager->flush();
-//            array_push($arrayInstructor, $instructor);
-//        }
-//
-//        //Student
-//        for ($i=0; $i<50; $i++){
-//            $name = $faker->name();
-//            $arrayName = explode(' ', $name);
-//            $student = new Student();
-//
-//            $student->setFirstname($arrayName[0]);
-//            $student->setLastname($arrayName[1]);
-//            $student->setIdModule('12345');
-//            $student->setMail3wa('xxxxx@3wa.io');
-//            $student->setBirthDate(new \DateTime("22-05-1997"));
-//
-//            $student->setBadges([
-//                array_rand($arrayModule,1) => "Découvre",
-//                array_rand($arrayModule,1) => "Explore",
-//                array_rand($arrayModule,1) => "Domine",
-//            ]);
-//
-//            $manager->persist($student);
-//            array_push($arrayStudent, $student);
-////
-////
-////            //LinkSessionStudent
-//            $lms = new linkSessionStudent();
-//            $lms->setEnabled(1);
-//            $lms->setStudent($student);
-//            $lms->setSession($arraySession[rand(1,9)]);
-//
-//            $manager->persist($lms);
-////            $manager->flush();
-//        }
+        //Module
+        for ($i=0; $i<10; $i++)
+        {
+            $word = $faker->word();
+            $module = new Module();
+            $module->setTitle($word);
+            $module->setNumberOfWeeks(rand(1,10));
 
+            $manager->persist($module);
+            $manager->flush();
+            array_push($arrayModule, $module);
+        }
 
+        //Session
+        for ($i=0; $i<10; $i++)
+        {
+            $word = $faker->word();
+            $session = new Session();
+            $session->setName($word);
+            $session->setSchoolYear(2021);
 
+            array_push($arraySession, $session);
+            $manager->persist($session);
+            $manager->flush();
+        }
+
+        //LinkSessionModule
+        foreach( $arrayModule as $module )
+        {
+            $linkSessionModule = new LinkSessionModule();
+            $linkSessionModule->setModule( $module );
+            $linkSessionModule->setSession( $arraySession[array_rand($arraySession)] );
+            $linkSessionModule->setStartDate( $faker->dateTimeBetween('-1 year', 'now') );
+            $linkSessionModule->setEndDate( $faker->dateTimeBetween( 'now', '+1 month' ) );
+            $manager->persist($linkSessionModule);
+            $manager->flush();
+        }
+
+        //Instructeur
+        for ($i=0; $i<10; $i++)
+        {
+            $name = $faker->name();
+            $arrayName = explode(' ', $name);
+            $instructor = new Instructor();
+
+            $instructor->setFirstname($arrayName[0]);
+            $instructor->setLastname($arrayName[1]);
+            $instructor->setBirthDate(new \DateTime("22-05-1957"));
+            $instructor->setPhoneNumber('06xxxxxxxxxx');
+            $instructor->setEmail($faker->email());
+            $instructor->setPassword(
+                $this->userPasswordHasherInterface->hashPassword(
+                    $instructor, "password"
+                )
+            );
+            $instructor->setRoles(['instructor']);
+
+            $instructorModule = $arrayModule[array_rand($arrayModule)];
+            $instructor->addModule($instructorModule);
+
+            // $sessionInstructor = $arraySession[array_rand($arraySession)];
+            // $instructor->addSession($sessionInstructor);
+
+            array_push($arrayInstructor, $instructor);
+            $manager->persist($instructor);
+            $manager->flush();
+        }
+
+        //Student
+        for ($i=0; $i<20; $i++)
+        {
+            $name = $faker->name();
+            $arrayName = explode(' ', $name);
+            $student = new Student();
+
+            $student->setFirstname($arrayName[0]);
+            $student->setLastname($arrayName[1]);
+            $student->setIdModule('12345');
+            $student->setMail3wa('xxxxx@3wa.io');
+            $student->setBirthDate(new \DateTime("22-05-1997"));
+
+            $student->setBadges([
+                array_rand($arrayModule,1) => "Découvre",
+                array_rand($arrayModule,1) => "Explore",
+                array_rand($arrayModule,1) => "Domine",
+            ]);
+
+            array_push($arrayStudent, $student);
+            $manager->persist($student);
+            $manager->flush();
+
+            //LinkSessionStudent
+            $lms = new linkSessionStudent();
+            $lms->setEnabled(1);
+            $lms->setStudent($student);
+            $lms->setSession($arraySession[array_rand($arraySession)]);
+
+            $manager->persist($lms);
+            $manager->flush();
+        }
 
         //Question
-//        for ($i=0; $i<30; $i++){
-//
-//            $text = $faker->sentence();
-//            $question = new Question();
-//
-//            $question->setWording($text);
-//            $instructorEntity= $this->instructorRepository->findAll();
-//            $instructorEntity = $instructorEntity[rand(0,9)];
-//            $instructorId = $instructorEntity->getId();
-//            $question->setIdAuthor($instructorId);
-//
-//            $question->setEnabled(1);
-//            $question->setIsMandatory(0);
-//            $question->setIsOfficial(0);
-//
-//            $difficulty = $ChoicesDifficulty[rand(0,2)];
-//            $question->setDifficulty($difficulty);
-//
-//            $instructorModules = $instructorEntity->getModules();
-//            $question->setModule($instructorModules[0]);
-//            $question->setResponseType('checkbox');
-//
-//            $manager->persist($question);
-//
-//
-//        }
+        for ($i=0; $i<20; $i++)
+        {
+            $text = $faker->sentence();
+            $question = new Question();
+
+            $question->setWording($text);
+            $instructorEntity = $this->instructorRepository->findAll();
+            $instructorEntity = $instructorEntity[array_rand($instructorEntity)];
+            $instructorId = $instructorEntity->getId();
+            $question->setIdAuthor($instructorId);
+
+            $question->setEnabled(1);
+            $question->setIsMandatory(0);
+            $question->setIsOfficial(0);
+
+
+            $difficulty = $ChoicesDifficulty[rand(0,2)];
+            $question->setDifficulty($difficulty);
+
+            // $instructorModules = $instructorEntity->getModules();
+            $question->setModule($arrayModule[array_rand($arrayModule)]);
+            $question->setResponseType('checkbox');
+
+            $manager->persist($question);
+            $manager->flush();
+        }
 
         //Proposal
-            $count=0;
-//            $length = rand(0,6);
-//            $questions = $this->questionRepository->findAll();
-//
-//                for ($i=0; $i<50; $i++){
-//
-//                    $word = $faker->word();
-//                    $proposal = new Proposal();
-//
-//                    $proposal->setWording($word);
-//                    $isCorrect = rand(0,1);
-//                    $proposal->setIsCorrect($isCorrect);
-//                    if($isCorrect){
-//                        $count ++;
-//                    }
-//
-//                    $proposal->setQuestion($questions[rand(0,29)]);
-//                    $manager->persist($proposal);
-//                }
+        $count=0;
+        $length = rand(0,6);
+        $questions = $this->questionRepository->findAll();
+
+        for ($i=0; $i<20; $i++)
+        {
+            $word = $faker->word();
+            $proposal = new Proposal();
+
+            $proposal->setWording($word);
+            $isCorrect = rand(0,1);
+            $proposal->setIsCorrect($isCorrect);
+            if($isCorrect){
+                $count ++;
+            }
+
+            $proposal->setQuestion($questions[array_rand($questions)]);
+            $manager->persist($proposal);
+            $manager->flush();
+        }
 
         //Qcm
-        for ($i=0; $i<10; $i++){
+        for ($i=0; $i<5; $i++)
+        {
             $word = $faker->word();
             $qcm = new Qcm();
             $qcm->setEnabled('1');
@@ -226,7 +238,7 @@ class AppFixtures extends Fixture
 //                return $question->getModule() === $module;
 //            });
 
-            for($i=0; $i<10; $i++){
+            for($i=0; $i<5; $i++){
                 $question=$questions[array_rand($questions)];
                 $qcm->addQuestion($question);
                 $answers = $question->getProposals();
@@ -269,18 +281,7 @@ class AppFixtures extends Fixture
 
 
             $manager->persist($qcm);
-
+            $manager->flush();
         }
-
-        $manager->flush();
-
-
-
-
-
-
-
-
-
     }
 }
