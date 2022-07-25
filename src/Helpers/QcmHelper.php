7d004480255555e45f41ccc8f6957bf89e0c2dac
matth <matthieu.fergola@gmail.com>
2022-07-25 17:15:27 +0200

    update qcm author for user instead of instructor (for random qcm)

---
+++ src/Helpers/QcmHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helpers;
+
+class QcmHelper
+{
+    public static function generateRandomQcm( $module, $difficulty = 2 )
+    {
+        // title = "QCM - entrainement -  $module->getName() - " . ;
+    }
+
+    public static function generateOfficialQcm()
+    {
+
+    }
+}
