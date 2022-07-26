efc03346d089feed7bf0a5722d042104978df21a
matth <matthieu.fergola@gmail.com>
2022-07-26 16:59:03 +0200

    retouche entities 2

--- migrations/Version20220726145808.php
+++ migrations/Version20220726145808.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220726145115 extends AbstractMigration
+final class Version20220726145808 extends AbstractMigration
 {
     public function getDescription(): string
     {
