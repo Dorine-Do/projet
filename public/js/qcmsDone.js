9644e22dc42ac8ff105cf4ec4a4c37d03ee7a110
matth <matthieu.fergola@gmail.com>
2022-07-29 15:46:29 +0200

    add qcmDone js

---
+++ public/js/qcmsDone.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    let select = document.getElementById('select_module');
+
+    function getSelectValue(){
+        let selectedValue = select.options[select.selectedIndex].text
+        let module = document.querySelector('#module')
+        module.innerHTML = "Module : " + selectedValue
+    }
+
+
+
+    select.addEventListener('change', getSelectValue)
+
+})
