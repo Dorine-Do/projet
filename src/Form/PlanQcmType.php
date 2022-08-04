7f7fbb2a7111d13f0de7ce1b25a93567b7779526
matth <matthieu.fergola@gmail.com>
2022-08-04 11:13:44 +0200

    begin instructor plan QCM add TODOs

--- src/Form/PlanQcmType.php
+++ src/Form/PlanQcmType.php
@@ -12,14 +12,18 @@ class PlanQcmType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            // TODO: recupérer uniquement les session liées au formateur
             ->add('session', EntityType::class, [
                 'class' => Session::class,
                 'choices' => function() {
 
                 }
             ])
+            // TODO: recupérer les modules liés à cette session (dynamique)
             ->add('module')
+            // TODO: recupérer les QCM liés à ce module (dynamique)
             ->add('qcm')
+            // TODO: recupérer les Eleves liés à cette session (dynamique)
             ->add('student')
             ->add('startTime')
             ->add('endTime')
