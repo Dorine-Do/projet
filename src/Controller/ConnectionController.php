8a7170b554fb63b28ee15cf0372cfa120a8afd3f
iDecow <evan.collebrusco@3wa.io>
2022-08-24 11:18:52 +0200

    Pour pouvoir accéder aux pages sans google

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -10,42 +10,42 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class ConnectionController extends AbstractController
 {
-    #[Route('/', name: 'app_connection')]
-    public function index( AuthenticationUtils $authUtils ): Response
-    {
-        $error = $authUtils->getLastAuthenticationError();
-        $lastUsername = $authUtils->getLastUsername();
+//    #[Route('/', name: 'app_connection')]
+//    public function index( AuthenticationUtils $authUtils ): Response
+//    {
+//        $error = $authUtils->getLastAuthenticationError();
+//        $lastUsername = $authUtils->getLastUsername();
+//
+//        return $this->render('connection/index.html.twig', [
+//            'last_username' => $lastUsername,
+//            'error' => $error
+//        ]);
+//    }
 
-        return $this->render('connection/index.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error
-        ]);
-    }
-
-    #[Route('/dashboard/check', name: 'app_check_dashboard')]
-    public function roleChecking( Security $security): Response
-    {
-        $user = $security->getUser();
-
-        $userRoles = $user->getRoles();
-
-        if( in_array('ROLE_ADMIN', $userRoles) )
-        {
-            $dashboardRouteName = 'app_admin';
-        }
-        elseif( in_array('ROLE_INSTRUCTOR', $userRoles) )
-        {
-            $dashboardRouteName = 'welcome_instructor';
-        }
-        elseif( in_array('ROLE_STUDENT', $userRoles) )
-        {
-            $dashboardRouteName = 'student_qcms';
-        }
-        elseif( in_array('ROLE_USER', $userRoles) )
-        {
-            $dashboardRouteName = 'home';
-        }
-
-        return $this->redirectToRoute( $dashboardRouteName );
-    }
+//    #[Route('/dashboard/check', name: 'app_check_dashboard')]
+//    public function roleChecking( Security $security): Response
+//    {
+//        $user = $security->getUser();
+//
+//        $userRoles = $user->getRoles();
+//
+//        if( in_array('ROLE_ADMIN', $userRoles) )
+//        {
+//            $dashboardRouteName = 'app_admin';
+//        }
+//        elseif( in_array('ROLE_INSTRUCTOR', $userRoles) )
+//        {
+//            $dashboardRouteName = 'welcome_instructor';
+//        }
+//        elseif( in_array('ROLE_STUDENT', $userRoles) )
+//        {
+//            $dashboardRouteName = 'student_qcms';
+//        }
+//        elseif( in_array('ROLE_USER', $userRoles) )
+//        {
+//            $dashboardRouteName = 'home';
+//        }
+//
+//        return $this->redirectToRoute( $dashboardRouteName );
+//    }
 }
--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -1,51 +1,51 @@
 <?php
 
-namespace App\Controller;
-
-use KnpU\OAuth2ClientBundle\Client\Provider\GoogleClient;
-use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-use League\OAuth2\Client\Provider\GoogleUser;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use \Symfony\Component\HttpFoundation\RedirectResponse;
-
-
-class GoogleController extends AbstractController
-{
-    /**
-     * Link to this controller to start the "connect" process
-     *
-     */
-    #[Route("/connect/google", name:"connect_google")]
-    public function connectAction(ClientRegistry $clientRegistry): RedirectResponse
-    {
-        return $clientRegistry
-            ->getClient('google')
-            ->redirect([]);
-    }
-
-    /**
-     * After going to Google, you're redirected back here because this is the "redirect_route"
-     * you configured in config/packages/knpu_oauth2_client.yaml
-     */
-    #[Route("/connect/google/check", name:"connect_google_check")]
-    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
-    {
-        /** @var GoogleClient $client */
-        $client = $clientRegistry->getClient('google');
-
-        try {
-            /** @var GoogleUser $user */
-            $user = $client->fetchUser();
-
-            // e.g. $name = $user->getFirstName();
-            // var_dump($user); die;
-        } catch (IdentityProviderException $e) {
-            // something went wrong!
-            // probably you should return the reason to the user
-            var_dump($e->getMessage()); die;
-        }
-    }
-}
+//namespace App\Controller;
+//
+//use KnpU\OAuth2ClientBundle\Client\Provider\GoogleClient;
+//use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+//use League\OAuth2\Client\Provider\GoogleUser;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\Routing\Annotation\Route;
+//use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use \Symfony\Component\HttpFoundation\RedirectResponse;
+//
+//
+//class GoogleController extends AbstractController
+//{
+//    /**
+//     * Link to this controller to start the "connect" process
+//     *
+//     */
+//    #[Route("/connect/google", name:"connect_google")]
+//    public function connectAction(ClientRegistry $clientRegistry): RedirectResponse
+//    {
+//        return $clientRegistry
+//            ->getClient('google')
+//            ->redirect([]);
+//    }
+//
+//    /**
+//     * After going to Google, you're redirected back here because this is the "redirect_route"
+//     * you configured in config/packages/knpu_oauth2_client.yaml
+//     */
+//    #[Route("/connect/google/check", name:"connect_google_check")]
+//    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+//    {
+//        /** @var GoogleClient $client */
+//        $client = $clientRegistry->getClient('google');
+//
+//        try {
+//            /** @var GoogleUser $user */
+//            $user = $client->fetchUser();
+//
+//            // e.g. $name = $user->getFirstName();
+//            // var_dump($user); die;
+//        } catch (IdentityProviderException $e) {
+//            // something went wrong!
+//            // probably you should return the reason to the user
+//            var_dump($e->getMessage()); die;
+//        }
+//    }
+//}
