4223cea96d01a8702bb13f69b4a325c964dfc07a
iDecow <evan.collebrusco@3wa.io>
2022-09-06 14:49:36 +0200

    attrape dorine

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -178,6 +178,8 @@ class StudentController extends AbstractController
 
         $result = $request->query->all();
 
+//        dd($result);
+
         $countIsCorrectAnswer = 0;
 
         if( count($result) !== 0 )
