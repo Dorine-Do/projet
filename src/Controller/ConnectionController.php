502ffc40a9bcd798d712224c339d24f8301b0fcc
matth <matthieu.fergola@gmail.com>
2022-09-19 15:44:48 +0200

    init Google OneTap Connection

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -10,13 +10,25 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class ConnectionController extends AbstractController
 {
+//    #[Route('/', name: 'app_connection')]
+//    public function index( AuthenticationUtils $authUtils ): Response
+//    {
+//        $error = $authUtils->getLastAuthenticationError();
+//        $lastUsername = $authUtils->getLastUsername();
+//
+//        return $this->render('connection/index.html.twig', [
+//            'last_username' => $lastUsername,
+//            'error' => $error
+//        ]);
+//    }
+
     #[Route('/', name: 'app_connection')]
     public function index( AuthenticationUtils $authUtils ): Response
     {
         $error = $authUtils->getLastAuthenticationError();
         $lastUsername = $authUtils->getLastUsername();
 
-        return $this->render('connection/index.html.twig', [
+        return $this->render('connection/one-tap.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error
         ]);
@@ -47,5 +59,28 @@ class ConnectionController extends AbstractController
         }
 
         return $this->redirectToRoute( $dashboardRouteName );
+    }
+
+    #[Route('/connection/google-one-tap', name: 'app_onetap_connection')]
+    public function oneTapConnect()
+    {
+        $client = new \Google_Client();
+
+    }
+
+    #[Route('/connection/one-tap/check/{id_token}', name: 'app_onetap_connection_check')]
+    public function oneTapCheck( $id_token ): Response
+    {
+        $client = new \Google_Client(['client_id' => '939653659160-g2cn2cgin2ua2cgp33003np8quf1l7m1.apps.googleusercontent.com']);
+        $payload = $client->verifyIdToken($id_token);
+        if( $payload )
+        {
+            $userid = $payload['sub'];
+        }
+        else
+        {
+            // Invalid ID token
+        }
+        return $this->redirectToRoute('app_check_dashboard');
     }
 }
