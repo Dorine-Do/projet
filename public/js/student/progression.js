a025678ed346b34d91913415b89733dfa90f83f3
Dorine-Do <dorine.journet@laposte.net>
2022-09-22 13:29:35 +0200

    Student progression.css / js

--- public/js/student/progression.js
+++ public/js/student/progression.js
@@ -1,4 +1,4 @@
-let lineY, lineX, passOrNot, timeLine, containerTimeLine, diffLeft, left = null;
+let lineY, lineX, passOrNot, passOrNots, timeLine, containerTimeLine, diffLeft, left, divIsOfficials = null;
 
 function lineYManagment(){
     lineY = document.querySelectorAll('.lineY')
@@ -60,7 +60,7 @@ function divIsOfficialManagment(){
     } )
 
     // Positionnement des div isOfficialQcm
-    let divIsOfficials = document.querySelectorAll('.isOfficialQcm')
+    divIsOfficials = document.querySelectorAll('.isOfficialQcm')
     left = divIsOfficials[0].getBoundingClientRect().left
     // Convertion px to em
     left = left/16
@@ -117,19 +117,88 @@ function divIsOfficialManagment(){
 
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    lineX = document.querySelector('.lineX')
-    containerTimeLine = document.querySelector('.timeLine')
+function windowSize420(){
+    passOrNots.forEach( p => {
+        p.style.marginBottom = 2 + 'em'
+        p.style.marginTop = 0
+    } )
+    lineX.style.height = timeLine.getBoundingClientRect().height + 'px'
+    let divPrev = null
+    let levelMaxByModule = 0;
+    divIsOfficials = document.querySelectorAll('.isOfficialQcm')
+    console.log(document.querySelectorAll('.isOfficialQcm'))
+    for (let i =0; i < divIsOfficials.length + 1; i++){
+        if (i !== 0) {
+            if (i !== divIsOfficials.length){
+                // Si il n'y a pas de changement de module
+                if (divPrev === divIsOfficials[i].querySelector('.moduleTitle').dataset.id) {
+                    if (i === 1) {
+                        divPrev = divIsOfficials[i].querySelector('.moduleTitle').dataset.id
+                    } else if (i !== 0) {
+                        divPrev = divIsOfficials[i].querySelector('.moduleTitle').dataset.id
+                    }
+                    if (levelMaxByModule < divIsOfficials[i]){
+                        levelMaxByModule = divIsOfficials[i].dataset.level
+                    }
+                }
+                    // Si il y a un changement de module, afficher le logo YouUp si l'étudiant à réussi à avoir la moyenne au moins
+                // une fois aux qcms de fin de semaine.
+                else {
+                    console.log(divIsOfficials[i])
+                    let pBadge = createInterCaseBagde(levelMaxByModule)
+                    divIsOfficials[i].parentNode.insertBefore(pBadge,divIsOfficials[i])
+                    divPrev = divIsOfficials[i].querySelector('.moduleTitle').dataset.id
+                    i --
+                }
+            }
+                // Dernière Div (ligne 46 : divIsOfficials.length + 1). Le +1 sert à verifier si le dernier qcm est le dernier
+                // du module de celui-ci.
+            // Si oui alors une imageBadge doit être afficher.
+            else{
+                let now = new Date()
+                let endDate = new Date(divIsOfficials[i-1].dataset.enddate)
+                if (endDate < now){
+                    let pBadge = createInterCaseBagde(levelMaxByModule,left)
+                    divIsOfficials[i-1].parentNode.append(pBadge)
+                }
+            }
+        }
+    }
+}
+
+function windowSizeSup420(){
     let leftLastElement = timeLine.lastElementChild.getBoundingClientRect().x / 16
     let leftFirstElement = timeLine.firstElementChild.getBoundingClientRect().x / 16
     divIsOfficialManagment()
     diffLeft = leftLastElement - leftFirstElement
     timeLine.style.width = diffLeft - 10 + 'em'
-
-    passOrNot = document.querySelector('.passOrNot')
-    timeLine = document.querySelector('.timeLine')
     lineXManagment()
     lineYManagment()
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    passOrNots = document.querySelectorAll('.passOrNot')
+    lineX = document.querySelector('.lineX')
+    containerTimeLine = document.querySelector('.timeLine')
+    timeLine = document.querySelector('.timeLine')
+    passOrNot = document.querySelector('.passOrNot')
+
+
+    if (window.innerWidth > 420){
+        windowSizeSup420()
+    }else if(window.innerWidth <= 420){
+        windowSize420()
+    }
+
+    window.addEventListener('resize', function(e) {
+        console.log(window.innerWidth)
+        timeLine = document.querySelector('.timeLine')
+        if (window.innerWidth > 420){
+            windowSizeSup420()
+        }else if(window.innerWidth <= 420){
+            windowSize420()
+        }
+    }, true);
 
 
 });
