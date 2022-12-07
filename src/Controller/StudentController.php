5afe45532c99ff9aeb612557357547b71ccb2cf2
matth <matthieu.fergola@gmail.com>
2022-12-07 14:25:51 +0100

    debug difficulty for training QCMs generation

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -223,124 +223,9 @@ class StudentController extends AbstractController
         $questionsCache = $qcm->getQuestionsCache();
 
         $resultRequest = $request->query->all();
-//        $countIsCorrectAnswer = 0;
 
         if( count($resultRequest) !== 0 )
         {
-//            foreach ( $questionsCache as $questionCacheKey => $questionCache )
-//            {
-//                foreach ( $resultRequest as $studentAnswerKey => $studentAnswerValue )
-//                {
-//                    if( $questionsCache[$questionCacheKey]['id'] == $studentAnswerKey )
-//                    {
-//                        // Radio
-//                        if ( !$questionsCache[$questionCacheKey]['isMultiple'] )
-//                        {
-//                            $radioIsCorrect = 0;
-//                            $studentAnswerValue = intval($studentAnswerValue);
-//                            foreach ($questionsCache[$questionCacheKey]['proposals'] as $proposalKey => $proposal)
-//                            {
-//                                //Si case cochée par l'etudiant et bonne réponse
-//                                if(
-//                                    $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['isCorrectAnswer']
-//                                    &&
-//                                    $studentAnswerValue === $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['id']
-//                                )
-//                                {
-//                                    $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['isStudentAnswer'] = 1;
-//                                    $radioIsCorrect ++ ;
-//                                }
-//                                // Si case cochée par l'etudiant
-//                                elseif(
-//                                    !$questionsCache[$questionCacheKey]['proposals'][$proposalKey]['isCorrectAnswer']
-//                                    &&
-//                                    $studentAnswerValue === $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['id']
-//                                )
-//                                {
-//                                    $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['isStudentAnswer'] = 1;
-//                                }
-//                                // Si pas case cochée par l'etudiant
-//                                else
-//                                {
-//                                    $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['isStudentAnswer'] = 0;
-//                                }
-//                            }
-//                            // Si l'étudiant a répondu juste
-//                            if ($radioIsCorrect !== 0)
-//                            {
-//                                $countIsCorrectAnswer++;
-//                                $questionsCache[$questionCacheKey]['student_answer_correct'] = 1;
-//                            }
-//                            else
-//                            {
-//                                $questionsCache[$questionCacheKey]['student_answer_correct'] = 0;
-//                            }
-//                        } // CheckBox
-//                        else
-//                        {
-//                            $dbAnswersCheck = [
-//                                'good' => [],
-//                                'bad' => []
-//                            ];
-//                            foreach( $questionsCache[$questionCacheKey]['proposals'] as $proposalKey => $proposal )
-//                            {
-//                                if( $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['isCorrectAnswer'] )
-//                                {
-//                                    $dbAnswersCheck['good'][] = $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['id'];
-//                                }
-//                                else
-//                                {
-//                                    $dbAnswersCheck['bad'][] = $questionsCache[$questionCacheKey]['proposals'][$proposalKey]['id'];
-//                                }
-//                            }
-//                            $goodAnswersCount = 0;
-//                            $badAnswersCount = 0;
-//                            foreach ($studentAnswerValue as $studentAnswer)
-//                            {
-//                                if( in_array( $studentAnswer, $dbAnswersCheck['good'] ) )
-//                                {
-//                                    $goodAnswersCount++;
-//
-//                                }
-//                                elseif( in_array($studentAnswer, $dbAnswersCheck['bad']) )
-//                                {
-//                                    $badAnswersCount++;
-//
-//                                }
-//                                else{
-//                                    $badAnswersCount++;
-//
-//                                }
-//                            }
-//
-//                            // Pour savoir quelles réponses à coché l'étudiant
-//                            foreach ($questionsCache[$questionCacheKey]['proposals'] as $answerDbKey => $answerDbValue)
-//                            {
-//                                if( in_array($answerDbValue['id'], $studentAnswerValue) )
-//                                {
-//                                    $questionsCache[$questionCacheKey]['proposals'][$answerDbKey]['isStudentAnswer'] = 1;
-//                                }
-//                                else
-//                                {
-//                                    $questionsCache[$questionCacheKey]['proposals'][$answerDbKey]['isStudentAnswer'] = 0;
-//                                }
-//                            }
-//
-//                            if( $goodAnswersCount === count($dbAnswersCheck['good']) && $badAnswersCount === 0 )
-//                            {
-//                                $countIsCorrectAnswer ++;
-//                                $questionsCache[$questionCacheKey]['student_answer_correct'] = 1;
-//                            }else{
-//                                $questionsCache[$questionCacheKey]['student_answer_correct'] = 0;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            $nbQuestions = count($questionsCache);
-//            $totalScore = (100/$nbQuestions)*$countIsCorrectAnswer;
-
             $totalScoreAndCache = QcmResultHelper::calcQcmPonderatedScore( $qcm, $resultRequest );
             $totalScore = $totalScoreAndCache['totalScore'];
             $questionsCache = $totalScoreAndCache['questionsCache'];
@@ -433,21 +318,19 @@ class StudentController extends AbstractController
         ]);
     }
 
-    #[Route('student/qcm/training', name: 'student_qcm_training', methods: ['GET']) ]
+    #[Route('student/qcm/training/{module}/{difficulty}', name: 'student_qcm_training', methods: ['GET']) ]
     public function qcmTraining(
-        Request $request,
-        ModuleRepository $moduleRepo,
         QuestionRepository $questionRepo,
         Security $security,
         EntityManagerInterface $manager,
-        UserRepository $userRepository
+        UserRepository $userRepository,
+        Module $module,
+        $difficulty = 2
     ): Response
     {
-        $module = $moduleRepo->find( $request->get('module') );
+        $difficulty = intval( $difficulty );
 
-        $difficulty = (int) $request->get('difficulty');
-
-        $student = $this->studentRepo->find($this->security->getUser()->getId());
+        $student = $this->security->getUser();
 
         $qcmGenerator = new QcmGeneratorHelper( $questionRepo, $security);
         $trainingQcm = $qcmGenerator->generateRandomQcm( $module, $student, $userRepository ,$difficulty, 'training');
