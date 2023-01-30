1e930e70143c67488961a8ed991ced84e8d58e97
Dorine-Do <dorine.journet@laposte.net>
2023-01-30 16:36:31 +0100

    dd cookies pour débug

--- src/Helpers/DbUpdaterHelper.php
+++ src/Helpers/DbUpdaterHelper.php
@@ -232,7 +232,7 @@ class DbUpdaterHelper
         if( $this->isUserInstructor( $user ) )
         {
             $instructorSuiviSessions = $this->getSuiviInstructorSessions( $user->getEmail() );
-            dd($instructorSuiviSessions);
+
             foreach( $instructorSuiviSessions as $instructorSuiviSession )
             {
                 // Sessions
