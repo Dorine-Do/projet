f9ec3ea4128679b1367ba7c3a742086e1e528502
Dorine-Do <dorine.journet@laposte.net>
2023-01-30 11:35:52 +0100

    Logs

---
+++ src/Entity/Main/Log.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity\Main;
+
+use App\Repository\LogRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LogRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Log
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $message = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $context = [];
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $level = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $levelName = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class, cascade: ["persist"] , inversedBy: 'logs')]
+    #[ORM\JoinColumn(nullable: true)]
+    private $user;
+
+    #[ORM\Column(type: 'json', nullable: true)]
+    private array $extra = [];
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue():void
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+
+    public function setContext(array $context): self
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getLevelName(): ?string
+    {
+        return $this->levelName;
+    }
+
+    public function setLevelName(string $levelName): self
+    {
+        $this->levelName = $levelName;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getExtra(): array
+    {
+        return $this->extra;
+    }
+
+    public function setExtra(?array $extra): self
+    {
+        $this->extra = $extra;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
