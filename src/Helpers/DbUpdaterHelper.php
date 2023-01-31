533a6655ea85fb572fc9a6fb8460b9e55f4b114c
Dorine-Do <dorine.journet@laposte.net>
2023-01-31 14:30:53 +0100

    dd to debug

--- src/Helpers/DbUpdaterHelper.php
+++ src/Helpers/DbUpdaterHelper.php
@@ -614,7 +614,7 @@ class DbUpdaterHelper
             LEFT JOIN daily ON daily.id_module = modules.id
             LEFT JOIN sessions ON sessions.id = daily.id_session
             WHERE daily.date >= NOW() - INTERVAL 1 YEAR
-            AND sessions.name LIKE ?
+            AND sessions.name = ?
             GROUP BY modules.name";
 
         $suiviModules = $this->rawSqlRequestToExtDb($modulesSql, [$sessionName]);
