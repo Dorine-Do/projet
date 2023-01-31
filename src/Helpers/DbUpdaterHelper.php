476d2f47ed2607d33573fd52f26b94bf8c99d756
Dorine-Do <dorine.journet@laposte.net>
2023-01-31 14:06:57 +0100

    dd to debug

--- src/Helpers/DbUpdaterHelper.php
+++ src/Helpers/DbUpdaterHelper.php
@@ -613,7 +613,7 @@ class DbUpdaterHelper
             FROM modules
             LEFT JOIN daily ON daily.id_module = modules.id
             LEFT JOIN sessions ON sessions.id = daily.id_session
-            WHERE LOWER(sessions.name) = ?
+            WHERE LCASE(sessions.name) = ?
             AND daily.date >= NOW() - INTERVAL 1 YEAR
             GROUP BY modules.name";
 
